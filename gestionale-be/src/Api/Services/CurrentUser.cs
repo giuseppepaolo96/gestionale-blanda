376572147cfca2caea2a7301368176ade1e46b0c
Ang310-93 <60385801+Ang310-93@users.noreply.github.com>
2025-10-23 12:26:50 +0200

    Provo aggiungendo CurrentUser senza cambiare il Program.cs

---
+++ gestionale-be/src/Api/Services/CurrentUser.cs
@@ -0,0 +1,19 @@
+using Microsoft.AspNetCore.Http;
+using System.Security.Claims;
+
+namespace Api.Services
+{
+    public class CurrentUser : IUser
+    {
+        private readonly IHttpContextAccessor _httpContextAccessor;
+
+        public CurrentUser(IHttpContextAccessor httpContextAccessor)
+        {
+            _httpContextAccessor = httpContextAccessor;
+        }
+
+        public string? UserId => _httpContextAccessor.HttpContext?.User?.FindFirstValue(ClaimTypes.NameIdentifier);
+
+        public string? UserName => _httpContextAccessor.HttpContext?.User?.Identity?.Name;
+    }
+}
