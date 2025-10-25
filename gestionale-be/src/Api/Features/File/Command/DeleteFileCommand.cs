27058ab8ef239d675ebd220afa3b4a3d2d3bc9b6
giuseppepaolo96 <giuseppepaolo96.gp@gmail.com>
2025-10-25 14:28:44 +0200

    Aggiunta delete tabella FileRecords

---
+++ gestionale-be/src/Api/Features/File/Command/DeleteFileCommand.cs
@@ -0,0 +1,27 @@
+﻿using Api.Infrastructure.Data;
+using Api.Services;
+using FastEndpoints;
+
+namespace Api.Features.File.Command
+{
+    public class DeleteFileCommand : ICommand
+    {
+        public int FileRecordsId { get; set; }
+    }
+
+    public class DeleteFileCommandHandler : ICommandHandler<DeleteFileCommand>
+    {
+        private readonly AppDbContext _dbContext;
+        private readonly DeleteService _deleteService;
+
+        public DeleteFileCommandHandler (DeleteService deleteService)
+        {
+            _deleteService = deleteService;
+        }
+
+        public async Task ExecuteAsync(DeleteFileCommand command,CancellationToken ct)
+        {
+            await _deleteService.DeleteFileAsync(command.FileRecordsId, ct);
+        }
+    }
+}
---
+++ gestionale-be/src/Api/Features/File/Endpoint/DeleteFileEndpoint.cs
@@ -0,0 +1,39 @@
+﻿using Api.Features.File.Command;
+using Api.Infrastructure.Data;
+using FastEndpoints;
+using Microsoft.EntityFrameworkCore;
+
+namespace Api.Features.File.Endpoint
+{
+    public class DeleteFileEndpoint : Endpoint<DeleteFileCommand>
+    {
+        private readonly AppDbContext _dbContext;
+
+        public DeleteFileEndpoint(AppDbContext dbContext)
+        {
+            _dbContext = dbContext;
+        }
+
+        public override void Configure()
+        {
+            Delete("/file/{FileRecordsId}");
+            AllowAnonymous();
+        }
+
+        public override async Task HandleAsync(DeleteFileCommand command, CancellationToken ct)
+        {
+            Console.WriteLine($"Received FileRecordsId:{command.FileRecordsId}");
+            var fileRecord = await _dbContext.FileRecords
+                .FirstOrDefaultAsync(t => t.Id == command.FileRecordsId, ct);
+            if (fileRecord == null)
+            {
+                throw new Exception("File non presente");
+            }
+
+            _dbContext.FileRecords.Remove(fileRecord);
+            await _dbContext.SaveChangesAsync(ct);
+
+            await SendOkAsync();
+        }
+    }
+}
